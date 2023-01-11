007476d267149a236a7b8277a0f4ab8fb70dd472
efimov4242 <64278427+efimov4242@users.noreply.github.com>
2023-01-11 12:56:29 +0400

    Update 01.py

--- 01.py
+++ 01.py
@@ -50,58 +50,66 @@ write_file(create_str(koef))
 
 
 
-# A. Задана натуральная степень k. Сформировать случайным образом список \
-#     коэффициентов (значения от 0 до 100) многочлена и записать в файл \
-#         многочлен степени k.
-# Пример:
-#     k=2 => 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10*x² = 0.
-
-import random
+# B. Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
 
 
-def write_file_one(st):
-    with open('file1.txt', 'w') as data:
-        data.write(st)
+def replace(equation):
+    dict = {}
+    equation = equation.replace(' - ', ' -').replace(' + ', ' +')
+    equation = equation.split()
+    equation = equation[:-2]
+    for i in range(len(equation)):
+        equation[i] = equation[i].replace('+', '').split('x**')
+        dict[int(equation[i][1])] = int(equation[i][0])
+    return dict
 
-def write_file_two(st):
-    with open('file2.txt', 'w') as data:
-        data.write(st)
+def sumEquation(dict1, dict2):
+    dictResult = {}
+    maximum = (max(max(dict1), max(dict2)))
+    for i in range(maximum, -1, -1):
+        first = dict1.get(i)
+        second = dict2.get(i)
+        if first != None or second != None:
+            dictResult[i] = (first if first != None else 0) + (second if second != None else 0)
+    return dictResult
 
-def rnd():
-    return random.randint(0, 101)
-
-
-def create_mn(k):
-    lst = [rnd() for i in range(k+1)]
-    return lst
-
-
-def create_str(sp):
-    lst = sp[::-1]
-    wr = ''
-    if len(lst) < 1:
-        wr = 'x = 0'
-    else:
-        for i in range(len(lst)):
-            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
-                wr += f'{lst[i]}x^{len(lst)-i-1}'
-                if lst[i+1] != 0:
-                    wr += ' + '
-            elif i == len(lst) - 2 and lst[i] != 0:
-                wr += f'{lst[i]}x'
-                if lst[i+1] != 0:
-                    wr += ' + '
-            elif i == len(lst) - 1 and lst[i] != 0:
-                wr += f'{lst[i]} = 0'
-            elif i == len(lst) - 1 and lst[i] == 0:
-                wr += ' = 0'
-    return wr
+def result(dictResult):
+    result = ''
+    for i in dictResult.items():
+        if result == '':
+            if i[1] < 0:
+                result += ' - ' + str(abs(i[1])) + 'x^' + str(abs(i[0]))
+            elif i[1] > 0:
+                result += str(abs(i[1])) + 'x^' + str(abs(i[0]))
+        else:
+            if i[1] < 0:
+                result += ' - ' + str(abs(i[1])) + 'x^' + str(abs(i[0]))
+            elif i[1] > 0:
+                result += ' + ' + str(abs(i[1])) + 'x^' + str(abs(i[0]))
+        result = result.replace('x^1', 'x').replace('x^0', '').replace('1x^', 'x^')
+    return result + ' = 0'
 
 
-k1 = int(input("Введите натуральную степень k1 = "))
-k2 = int(input("Введите натуральную степень k2 = "))
-koef1 = create_mn(k1)
-koef2 = create_mn(k2)
-write_file_one(create_str(koef1))
-write_file_two(create_str(koef2))
+with open('file1.txt', 'r', encoding='utf-8') as text:
+    equation = text.readline()
+with open('file2.txt', 'r', encoding='utf-8') as text:
+    equation2 = text.readline()
+
+print(equation)
+print(equation2)
+
+dict1 = replace(equation)
+dict2 = replace(equation2)
+
+dictResult = sumEquation(dict1, dict2)
+print(dictResult)
+
+print(dict1)
+print(dict2)
+
+dictResult = result(dictResult)
+print(dictResult)
+
+with open('file3.txt', 'w', encoding='utf-8') as text:
+    text.writelines(dictResult)
 
